8e8b0dc000a9bdc2b4f908d4761f7b32e3e1dfaf
PlayJeri <numri.jeri@gmail.com>
2023-11-27 15:14:55 +0200

    return types added

---
+++ backend/src/types/Station.ts
@@ -0,0 +1,30 @@
+export type StationData = {
+    stationId: number;
+    name: string;
+    address: string;
+    coordinates: {
+        x: number;
+        y: number;
+    };
+};
+
+export type StationDetails = {
+    stationData: StationData;
+    journeysStarted: number;
+    journeysEnded: number;
+    avgJourneyDistance: number;
+    avgJourneyDuration: number;
+    topThreeDestinations: topThreeDestination[];
+    peakTimes: peakTimes[];
+};
+
+type peakTimes = {
+    hour: number;
+    journeysStarted: number;
+};
+
+type topThreeDestination = {
+    stationId: number;
+    stationName: string;
+    numberOfJourneys: number;
+};
