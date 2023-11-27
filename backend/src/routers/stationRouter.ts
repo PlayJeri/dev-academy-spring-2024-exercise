73769c784566c3c67996e7f243b1a9ba8103afcc
PlayJeri <numri.jeri@gmail.com>
2023-11-27 15:15:42 +0200

    single station data endpoint added

--- backend/src/routers/stationRouter.ts
+++ backend/src/routers/stationRouter.ts
@@ -4,5 +4,6 @@ import controllers from "../controllers";
 const router = Router();
 
 router.get("/", controllers.station.getAllStations);
+router.get("/:id", controllers.station.getStationById);
 
 export { router as stationRouter };
