09a8fbb64da3a0b826cd9981eb9e237a8804283f
PlayJeri <numri.jeri@gmail.com>
2023-12-04 15:44:20 +0200

    controller and /stats endpoint added

--- backend/src/routers/journeyRouter.ts
+++ backend/src/routers/journeyRouter.ts
@@ -1,5 +1,9 @@
 import { Router } from "express";
+import controllers from "../controllers";
 
 const router = Router();
+const controller = controllers.journey;
+
+router.get("/stats", controller.getJourneyStats);
 
 export { router as journeyRouter };
