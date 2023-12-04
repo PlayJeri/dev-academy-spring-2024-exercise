e824a23189e6d951a077bf84ae8af5d4ee84507c
PlayJeri <numri.jeri@gmail.com>
2023-12-04 17:16:40 +0200

    types added

---
+++ frontend/src/types/index.ts
@@ -0,0 +1,10 @@
+export type InfoBannerData = {
+    numberOfStations: string;
+    numberOfJourneys: string;
+    totalDistance: string;
+};
+
+export type JourneyStats = {
+    totalDistance: number;
+    numberOfJourneys: number;
+};
