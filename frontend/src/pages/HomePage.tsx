0d542f37ecdbf69636f5b7f16d8d5a53f8b51800
PlayJeri <numri.jeri@gmail.com>
2023-12-04 17:15:06 +0200

    infoBannerData state and fetching added

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,11 +1,37 @@
 import { HomePageInfoCard } from "../components/HomePageInfoCard";
 import { StatInfoBanner } from "../components/StatInfoBanner";
+import { useState, useEffect } from "react";
+import { InfoBannerData, JourneyStats } from "../types";
+import axios from "axios";
 
 export const HomePage = () => {
+    const [infoBannerData, setInfoBannerData] = useState<InfoBannerData>();
+
+    useEffect(() => {
+        const fetchInfoBannerData = async () => {
+            const numberOfStations = await axios.get("/station");
+            const journeyData: JourneyStats = (
+                await axios.get("/journey/stats")
+            ).data;
+
+            setInfoBannerData({
+                numberOfStations: numberOfStations.data.length,
+                numberOfJourneys: Number(
+                    journeyData.numberOfJourneys
+                ).toLocaleString(),
+                totalDistance: Math.round(
+                    journeyData.totalDistance / 1000
+                ).toLocaleString(),
+            });
+        };
+
+        fetchInfoBannerData();
+    }, []);
+
     return (
         <div className="flex flex-col items-center h-screen">
             <HomePageInfoCard />
-            <StatInfoBanner />
+            {infoBannerData && <StatInfoBanner stats={infoBannerData} />}
         </div>
     );
 };
