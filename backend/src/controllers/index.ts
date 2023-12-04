2cca88a108a695be618b4b94bda807dc2b2d8964
PlayJeri <numri.jeri@gmail.com>
2023-12-04 15:43:48 +0200

    journey and getJourneyStats added

--- backend/src/controllers/index.ts
+++ backend/src/controllers/index.ts
@@ -1,8 +1,12 @@
 import { getAllStations, getStationById } from "./stationController";
+import { getJourneyStats } from "./journeyControllers";
 
 export default {
     station: {
         getAllStations,
         getStationById,
+    },
+    journey: {
+        getJourneyStats,
     },
 };
