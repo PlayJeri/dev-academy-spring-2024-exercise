14a468a2ab5995e84e912fd2a7ead78110fdd4e4
PlayJeri <numri.jeri@gmail.com>
2023-12-17 11:14:01 +0200

    Add AllStationsPage route to App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
-import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
+import { AllStationsPage } from "./pages/AllStationsPage";
 import { NavBar } from "./components/NavBar";
 
 export default function App() {
@@ -9,12 +9,8 @@ export default function App() {
             <NavBar />
             <Routes>
                 <Route path="/" element={<HomePage />} />
+                <Route path="/stations" element={<AllStationsPage />} />
             </Routes>
         </BrowserRouter>
     );
 }
-
-const rootElement = document.getElementById("root");
-if (!rootElement) throw new Error("Root element not found");
-const root = ReactDOM.createRoot(rootElement);
-root.render(<App />);
