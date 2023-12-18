852c2d9721be768684fc38e7cdb422808e88d774
PlayJeri <numri.jeri@gmail.com>
2023-12-18 16:01:43 +0200

    Refactor AllStationsPage component

--- frontend/src/pages/AllStationsPage.tsx
+++ frontend/src/pages/AllStationsPage.tsx
@@ -5,24 +5,19 @@ import { StationData } from '../types/'
 
 export const AllStationsPage = () => {
     const [stations, setStations] = useState<StationData[]>([])
-    const [order, setOrder] = useState<"asc" | "desc">("desc");
 
     useEffect(() => {
         const fetchStations = async () => {
-            const stations = await axios.get(`/station?order=${order}`)
+            const stations = await axios.get(`/station`)
             setStations(stations.data)
         }
 
         fetchStations()
-    }, [order])
+    }, [])
 
   return (
     <div>
-      <h1>All Stations</h1>
-        <button onClick={() => setOrder(order === "asc" ? "desc": "asc")}>
-          {order === "asc" ? "Ascending" : "Descending"}
-        </button>
-        <StationList stations={stations} />
+        <StationList stations={stations} setStations={setStations} />
     </div>
   )
 }
