2f95bb0a58d0bf219627e0fb7012e31f57e78db3
PlayJeri <numri.jeri@gmail.com>
2023-12-19 18:50:39 +0200

    Update HomePageInfoCard styling

--- frontend/src/components/HomePageInfoCard.tsx
+++ frontend/src/components/HomePageInfoCard.tsx
@@ -2,7 +2,7 @@ import cityBikeImg from "../assets/city-bikes.jpeg";
 
 export const HomePageInfoCard = () => {
     return (
-        <div className="w-full py-12">
+        <div className="w-full py-8">
             <h1 className="py-5 text-4xl font-bold text-center">City Bikes</h1>
             <img
                 className="w-full max-w-[700px] mx-auto px-4"
@@ -10,17 +10,14 @@ export const HomePageInfoCard = () => {
                 alt="city bikes"
             />
             <p className="p-8">
-                Welcome to our City Bike service! 
-                We're proud to offer a convenient, 
-                eco-friendly transportation option 
-                that's loved by thousands of happy customers. 
-                With hundreds of stations scattered across the city, 
-                you're never far from your next ride. 
-                Our bikes are not only a green alternative 
-                that contributes to a healthier environment, 
-                but they also provide a fun and efficient way to navigate the city. 
-                Join us in making our city cleaner, greener, and more connected. 
-                Enjoy the ride!
+                Welcome to our City Bike service! We're proud to offer a
+                convenient, eco-friendly transportation option that's loved by
+                thousands of happy customers. With hundreds of stations
+                scattered across the city, you're never far from your next ride.
+                Our bikes are not only a green alternative that contributes to a
+                healthier environment, but they also provide a fun and efficient
+                way to navigate the city. Join us in making our city cleaner,
+                greener, and more connected. Enjoy the ride!
             </p>
         </div>
     );
