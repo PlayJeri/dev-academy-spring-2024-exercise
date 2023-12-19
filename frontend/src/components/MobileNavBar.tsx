1ddd3d8dfeb478a00841b98baf837c3f67d5c1d7
PlayJeri <numri.jeri@gmail.com>
2023-12-19 18:50:50 +0200

    Add MobileNavBar component and implement responsive navigation

---
+++ frontend/src/components/MobileNavBar.tsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+
+export const MobileNavBar = () => {
+    return (
+        <div className="flex flex-col sm:hidden items-center w-full gap-4 bg-blue-600">
+            <input
+                className="font-semibold text-slate-600  mt-4 w-3/4 rounded-md border-2 p-1 border-gray-300 focus:outline-none focus:border-blue-700"
+                type="text"
+                placeholder="Search..."
+            />
+            <div className="flex w-full justify-around text-white pb-4">
+                <h2 className="text-lg linkHover hover:text-xl font-light">
+                    <Link to={"/stations"}>Stations</Link>
+                </h2>
+                <h2 className="text-lg linkHover hover:text-xl font-light">
+                    Journeys
+                </h2>
+            </div>
+        </div>
+    );
+};
--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,18 +1,38 @@
 import { Link } from "react-router-dom";
 import BikeIcon from "../assets/BikeIcon.svg?react";
+import MenuIcon from "@mui/icons-material/Menu";
+import { MobileNavBar } from "./MobileNavBar";
+import { useState } from "react";
 
 export function NavBar() {
+    const [mobileNavOpen, setMobileNavOpen] = useState(false);
+
     return (
-        <div className="flex w-full bg-blue-600 text-slate-50 h-16 items-center px-8 justify-between sticky top-0">
-            <BikeIcon className="fill-blue-700 h-auto w-24 absolute z-0 left-0" />
-            <h1 className="font-light text-2xl z-10">City bikes</h1>
-            <h2 className="text-lg font-light"><Link to={"/stations"}>Stations</Link></h2>
-            <h2 className="text-lg font-light">Journeys</h2>
-            <input
-                className="rounded-md border-2 p-1 border-gray-300 focus:outline-none focus:border-blue-700"
-                type="text"
-                placeholder="Search..."
-            />
-        </div>
+        <>
+            <div className="flex w-full bg-blue-600 text-slate-50 h-16 items-center px-8 justify-between sticky top-0 z-10">
+                <BikeIcon className="fill-blue-700 h-auto w-24 absolute z-0 left-0" />
+                <h1 className="font-light text-2xl z-10 linkHover hover:text-3xl">
+                    <Link to={"/"}>City Bikes</Link>
+                </h1>
+                <h2 className="text-lg linkHover hover:text-xl font-light hidden sm:block">
+                    <Link to={"/stations"}>Stations</Link>
+                </h2>
+                <h2 className="text-lg linkHover hover:text-xl font-light hidden sm:block">
+                    Journeys
+                </h2>
+                <input
+                    className="hidden sm:block font-semibold text-slate-600 rounded-md border-2 p-1 border-gray-300 focus:outline-none focus:border-blue-700"
+                    type="text"
+                    placeholder="Search..."
+                />
+                <div
+                    className="block sm:hidden"
+                    onClick={() => setMobileNavOpen(!mobileNavOpen)}
+                >
+                    <MenuIcon fontSize="large" />
+                </div>
+            </div>
+            {mobileNavOpen && <MobileNavBar />}
+        </>
     );
 }
