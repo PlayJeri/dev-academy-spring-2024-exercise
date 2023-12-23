3a376f3e9a138bdcc56fefc3b3b71583a60c2144
PlayJeri <numri.jeri@gmail.com>
2023-12-23 11:30:38 +0200

    Add getAllJourneys endpoint to journeyControllers

--- backend/src/controllers/journeyControllers.ts
+++ backend/src/controllers/journeyControllers.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import pool from "../db";
-import { JourneyStats } from "../types/Journey";
+import { GetAllJourneysResponse, JourneyStats } from "../types/Journey";
 
 const db = pool.pool;
 
@@ -19,9 +19,45 @@ export const getJourneyStats = async (req: Request, res: Response) => {
             totalDistance: row.totaldistance,
             numberOfJourneys: row.journeycount,
         };
-        res.json(data);
+        return res.json(data);
     } catch (error) {
         console.error("Error in getJourneyStats", error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+};
+
+/**
+ * Get all journeys in the database
+ *
+ * @returns {GetAllJourneysResponse[]} - all journeys in the database
+ */
+export const getAllJourneys = async (req: Request, res: Response) => {
+    try {
+        console.log("getAllJourneys");
+        const { cursor, limit = 50, order = "DESC", filter } = req.query;
+        let query = "SELECT * FROM journey";
+
+        if (cursor) {
+            query += ` WHERE id > ${cursor}`;
+        }
+        if (filter) {
+            query += ` ORDER by ${filter} ${order}`;
+        }
+        query += ` LIMIT ${limit}`;
+        console.log(query);
+
+        const response = await db.query(query);
+        const data = response.rows;
+        const newCursor = data.length > 0 ? data[data.length - 1].id : null;
+
+        const result: GetAllJourneysResponse = {
+            nextCursor: newCursor,
+            journeys: data,
+        };
+
+        return res.json(result);
+    } catch (error) {
+        console.error("Error in getAllJourneys", error);
         res.status(500).json({ error: "Internal server error" });
     }
 };
