f7ddeea019141fefdb22c230d847fca78cca7d8b
PlayJeri <numri.jeri@gmail.com>
2023-12-23 11:30:03 +0200

    Add JourneyData type and GetAllJourneysResponse type

--- backend/src/types/Journey.ts
+++ backend/src/types/Journey.ts
@@ -2,3 +2,18 @@ export type JourneyStats = {
     totalDistance: number;
     numberOfJourneys: number;
 };
+
+type JourneyData = {
+    id: number;
+    departureDateTime: Date;
+    returnDateTime: Date;
+    departureStationId: number;
+    returnStationId: number;
+    distance: number;
+    duration: number;
+};
+
+export type GetAllJourneysResponse = {
+    nextCursor: number | null;
+    journeys: JourneyData[];
+};
