beb66ca81a33048a06adb5f47feeac4add8adc2a
PlayJeri <numri.jeri@gmail.com>
2024-01-03 14:40:22 +0200

    Add JourneyPage route to App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { HomePage } from "./pages/HomePage";
 import { AllStationsPage } from "./pages/AllStationsPage";
 import { NavBar } from "./components/NavBar";
 import { StationPage } from "./pages/StationPage";
+import { JourneyPage } from "./pages/JourneyPage";
 
 export default function App() {
     return (
@@ -12,6 +13,7 @@ export default function App() {
                 <Route path="/" element={<HomePage />} />
                 <Route path="/stations" element={<AllStationsPage />} />
                 <Route path="/station/:stationId" element={<StationPage />} />
+                <Route path="/journeys" element={<JourneyPage />} />
             </Routes>
         </BrowserRouter>
     );
