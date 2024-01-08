a2824f19188721dc73b9481b70b6a01729da02a8
PlayJeri <numri.jeri@gmail.com>
2024-01-08 14:55:42 +0200

    Update JourneyPage header style

--- frontend/src/pages/JourneyPage.tsx
+++ frontend/src/pages/JourneyPage.tsx
@@ -3,7 +3,7 @@ import { JourneysTable } from "../components/JourneysTable";
 export const JourneyPage = () => {
     return (
         <>
-            <h1>Journeys</h1>
+            <h1 className="text-3xl font-bold text-center py-4">Journeys</h1>
             <JourneysTable />
         </>
     );
