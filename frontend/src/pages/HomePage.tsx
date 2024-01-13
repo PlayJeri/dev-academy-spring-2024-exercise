3643bb1f7f46a00603047c0d1eb8814fa34ae6e0
PlayJeri <numri.jeri@gmail.com>
2024-01-13 17:45:37 +0200

    Add additional information to the StatInfoBanner component

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -27,7 +27,19 @@ export const HomePage = () => {
         <div className="flex flex-col items-center h-screen">
             <HomePageInfoCard />
             {infoBannerData ? (
-                <StatInfoBanner stats={infoBannerData} />
+                <div className="bg-neutral-800 text-yellow-500">
+                    <StatInfoBanner stats={infoBannerData} />
+                    <div className="my-4 p-8">
+                        <p>
+                            The data above provides some interesting insights
+                            into our service. Like the total number of stations
+                            across the city, total number of journeys made by
+                            our users and the total distance covered by all
+                            these journeys. We're proud to provide a service
+                            that helps people get where they need to go.
+                        </p>
+                    </div>
+                </div>
             ) : (
                 <p>Loading...</p>
             )}
